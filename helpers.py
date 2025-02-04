34b146bcc2eabcaa42c23e6ff24a1d283dfb6d2a
areynaldo <a.reynaldo.salina@gmail.com>
2025-02-04 18:01:07 +0100

    Helpers for drawing vectors etc. easier
    Co-authored-by: AdamDelle <AdamDelle@users.noreply.github.com>

---
+++ helpers.py
@@ -0,0 +1,20 @@
+import pygame
+
+RED = [255, 0, 0, 255]
+GREEN = [0, 255, 0, 255]
+BLUE = [0, 0, 255, 255]
+BLACK = [0, 0, 0, 255]
+
+def draw_vector(screen, v, origin, color=GREEN, width=2):
+    pygame.draw.line(screen, color, origin, origin+v, width)
+
+def draw_rect(screen, image, center, angle):
+    """
+    pass an input `image` created like:
+        image = pygame.Surface((1.5*10, 3*10))
+        image.set_colorkey(BLACK)   # key/gamma color
+        image.fill(BLUE)            # actual color of the rect
+    """
+    rotated = pygame.transform.rotate(image, angle)
+    rect = rotated.get_rect(center = center)
+    screen.blit(rotated, rect)
